a180c1c549abbe65f29d18ccabcc74b3566e89e2
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-10-21 10:36:11 -0300

    sofia

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -62,12 +62,12 @@ export class UsuarioService {
       this.token = localStorage.getItem('token');
       this.usuario = JSON.parse( localStorage.getItem('usuario'));
       this.menu = JSON.parse( localStorage.getItem('menu'));
-      this.sala = JSON.parse( localStorage.getItem('sala'));
+     //  this.sala = JSON.parse( localStorage.getItem('sala'));
     } else {
         this.token = '';
         this.usuario = null;
         this.menu = [];
-        this.sala = '';
+      //  this.sala = '';
       }
   }
 
